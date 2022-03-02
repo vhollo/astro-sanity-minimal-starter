665632d50486b1f42b64561f4c02150e616c59cf
vhollo <vhollo@protonmail.com>
2022-03-02 20:21:59 +0100

    US_styling

--- astro/tailwind.config.js
+++ astro/tailwind.config.js
@@ -11,6 +11,19 @@ module.exports = {
     //require('@tailwindcss/aspect-ratio'),
   ],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          primary: "#52b2d6",
+          "primary-content": "#ffffff",
+          //neutral: "#0b3b54",
+          "neutral-content": "#dfeef6",
+          "base-100": "#072c3f",
+          "base-content": "#dfeef6",
+          "--rounded-btn": "1.75rem",
+        },
+      },
+    ],
   },
 };
